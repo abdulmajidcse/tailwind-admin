b99c85979de55b0d1d594404eaa27d2c0f62e23f
Abdul Majid <abdulmajidcse@gmail.com>
2022-12-25 10:56:33 +0600

    update theme

--- src/dist/js/theme.js
+++ src/dist/js/theme.js
@@ -1,36 +1,27 @@
-// theme style switch
-function switchThemeStyles(theme) {
-  if (theme == "dark") {
-    document.documentElement.classList.add("dark");
-  } else {
-    document.documentElement.classList.remove("dark");
-  }
-}
-
 // set theme
 function setTheme(theme) {
   if (theme) {
     localStorage.setItem("theme", theme);
   } else if (!localStorage.getItem("theme")) {
-    localStorage.setItem("theme", "system");
+    localStorage.setItem(
+      "theme",
+      window.matchMedia("(prefers-color-scheme: dark)").matches
+        ? "dark"
+        : "light"
+    );
   }
 
-  const themeMenu = document.getElementById("theme_menu");
+  const lightThemeTag = document.getElementById("light_theme");
+  const darkThemeTag = document.getElementById("dark_theme");
 
-  if (localStorage.getItem("theme") == "system") {
-    themeMenu.innerHTML = `<i class="fas fa-adjust"></i> System`;
-
-    if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
-      switchThemeStyles("dark");
-    } else {
-      switchThemeStyles("light");
-    }
-  } else if (localStorage.getItem("theme") === "dark") {
-    themeMenu.innerHTML = `<i class="fas fa-moon"></i> Dark`;
-    switchThemeStyles("dark");
+  if (localStorage.getItem("theme") === "dark") {
+    document.documentElement.classList.add("dark");
+    darkThemeTag.classList.add("hidden");
+    lightThemeTag.classList.remove("hidden");
   } else {
-    themeMenu.innerHTML = `<i class="fas fa-sun"></i> Light`;
-    switchThemeStyles("light");
+    document.documentElement.classList.remove("dark");
+    lightThemeTag.classList.add("hidden");
+    darkThemeTag.classList.remove("hidden");
   }
 }
 
